f6a3a4b6ad5f6283aeecb43f0edb463c5da16180
Richard-zhang <rzsh2003@gmail.com>
2015-11-03 12:11:10 +0000

    zero

--- Calculus.hs
+++ Calculus.hs
@@ -37,24 +37,20 @@ eval (BinApp bin x y) env
 
 diff :: Exp -> String -> Exp
 diff (Val _) _
-  = Val 0
+  = 0
 diff (Id x) sym
-  | x == sym  = Val 1
-  | otherwise = Val 0
-diff (BinApp Add e1 e2) sym
-  = (BinApp Add (diff e1 sym) (diff e2 sym))
-diff (BinApp Mul e1 e2) sym
-  = (BinApp Add (BinApp Mul e1 (diff e2 sym)) (BinApp Mul (diff e1 sym) e2))
-diff (BinApp Div e1 e2) sym
-  = (BinApp Div (BinApp Add (BinApp Mul (diff e1 sym) (e2)) (UnApp Neg (BinApp Mul e1 (diff e2 sym)))) (BinApp Mul e2 e2))
-diff (UnApp Sin u) sym
-  = (BinApp Mul (UnApp Cos u) (diff u sym))
-diff (UnApp Cos u) sym
-  = (UnApp Neg (BinApp Mul (UnApp Sin u) (diff u sym)))
-diff (UnApp Log u) sym
-  = (BinApp Div (diff u sym) u)
-diff (UnApp Neg u) sym
-  = (UnApp Neg (diff u sym))
+  | x == sym  = 1
+  | otherwise = 0
+diff (BinApp op e1 e2) sym
+  | op == Add = (diff e1 sym) + (diff e2 sym)
+  | op == Mul = e1*(diff e2 sym) + (diff e1 sym)*e2
+  | op == Div = ((diff e1 sym)*e2+(-(e1*(diff e2 sym))))/(e2*e2)
+diff (UnApp op u) sym
+  | op == Sin = (cos u)*(diff u sym)
+  | op == Cos = -(sin u)*(diff u sym)
+  | op == Log = (diff u sym)/(u)
+  | op == Neg = -(diff u sym)
+
 
 maclaurin :: Exp -> Double -> Int -> Double
 maclaurin func val order
@@ -202,7 +198,7 @@ instance (Eq a, Fractional a) => Fractional (Maybe a) where
 
 
 
-
+{-}
 diff2 :: Exp -> String -> Maybe Exp
 diff2 (Val _) _
   = Nothing
@@ -223,19 +219,42 @@ diff2 (UnApp Log u) sym
   = (diff2 u sym)/(Just u)
 diff2 (UnApp Neg u) sym
   = -(diff2 u sym)
+-}
+diff2 :: Exp -> String -> Maybe Exp
+diff2 (Val _) _
+  = Nothing
+diff2 (Id x) sym
+  | x == sym = Just 1
+  | otherwise = Nothing
+diff2 (BinApp op e1 e2) sym
+  | op == Add = (diff2 e1 sym) + (diff2 e2 sym)
+  | op == Mul = (Just e1)*(diff2 e2 sym)+(diff2 e1 sym)*(Just e2)
+  | op == Div = ((diff2 e1 sym)*(Just e2)+(-((Just e1)*(diff2 e2 sym))))/(Just e2 * Just e2)
+diff2 (UnApp op u) sym
+  | op == Sin = (Just (cos u))*(diff2 u sym)
+  | op == Cos = -((Just (sin u))*(diff2 u sym))
+  | op == Log = (diff2 u sym)/(Just u)
+  | op == Neg = -(diff2 u sym)
 
 fastdiff :: Exp -> String -> Exp
-      
+fastdiff exp sym
+  = let myvalue = diff2 exp sym
+    in
+      case myvalue of
+        Just value -> value
+        Nothing    -> 0
+
 
 fastmaclaurin :: Exp -> Double -> Int -> Double
 fastmaclaurin func val order
   = sum (zipWith3 f a b c)
     where
       a = [val^n | n <- [0..(order-1)]]
-      b = iterate (flip diff "x") func
+      b = iterate (flip fastdiff "x") func
       c = map fromIntegral $ scanl (*) 1 [1..(order-1)]
       f value exp factor
         = eval (BinApp Div (BinApp Mul exp (Val value)) (Val factor)) [("x",0)]
+
 -- The following makes it much easier to input expressions, e.g. sin x, log(x*x) etc.
 {-
 x, y :: Exp
